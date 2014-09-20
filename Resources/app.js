ea2994584f3b6ee8f215267c1af55e7e14c14277
Efrén Valdez Jasso <jezrel_mx@MacBook-Pro-de-Efren.local>
2014-09-20 17:25:38 -0500

    ok

--- Resources/app.js
+++ Resources/app.js
@@ -9,7 +9,7 @@
  *
  */
 
-//bootstrap and check dependencies
+//bootstrap and check dependencies 
 if (Ti.version < 1.8) {
   alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
 }
