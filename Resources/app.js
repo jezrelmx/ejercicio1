74097d8220c50d04a0f23f0696f731d8d6b865b0
Efren Valdez <jezrelmx1304@gmail.com>
2014-09-21 08:32:24 -0500

    Interfaz con tabgroup y boton

--- Resources/app.js
+++ Resources/app.js
@@ -1,60 +1,59 @@
-/*
- * Single Window Application Template:
- * A basic starting point for your application.  Mostly a blank canvas.
- *
- * In app.js, we generally take care of a few things:
- * - Bootstrap the application with any data we need
- * - Check for dependencies like device type, platform version or network connection
- * - Require and open our top-level UI component
- *
- */
 
-//bootstrap and check dependencies codigo
-if (Ti.version < 1.8) {
-  alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
-}
+height = Ti.Platform.displayCaps.platformHeight,
+width = Ti.Platform.displayCaps.platformWidth;
 
-// This is a single context application with multiple windows in a stack
-(function() {
-  //render appropriate components based on the platform and form factor
-  var osname = Ti.Platform.osname,
-    version = Ti.Platform.version,
-    height = Ti.Platform.displayCaps.platformHeight,
-    width = Ti.Platform.displayCaps.platformWidth;
+console.log('alto '+height+' ancho '+width);
 
-  //considering tablets to have width over 720px and height over 600px - you can define your own
-  function checkTablet() {
-    var platform = Ti.Platform.osname;
+var miTabGroup = Ti.UI.createTabGroup();
 
-    switch (platform) {
-      case 'ipad':
-        return true;
-      case 'android':
-        var psc = Ti.Platform.Android.physicalSizeCategory;
-        var tiAndroid = Ti.Platform.Android;
-        return psc === tiAndroid.PHYSICAL_SIZE_CATEGORY_LARGE || psc === tiAndroid.PHYSICAL_SIZE_CATEGORY_XLARGE;
-      default:
-        return Math.min(
-          Ti.Platform.displayCaps.platformHeight,
-          Ti.Platform.displayCaps.platformWidth
-        ) >= 400
-    }
-  }
+var ventana1 = Ti.UI.createWindow({
+	title: 'ventana 1',
+	layout: 'vertical',
+	backgroundColor: '#F2F7FC',
+});
 
-  var isTablet = checkTablet();
-  console.log(isTablet);
+var pestana1 = Ti.UI.createTab({
+	title: 'Sentencia SQL',
+	window: ventana1,
+});
 
-  var Window;
-  if (isTablet) {
-    Window = require('ui/tablet/ApplicationWindow');
-  } else {
-    // Android uses platform-specific properties to create windows.
-    // All other platforms follow a similar UI pattern.
-    if (osname === 'android') {
-      Window = require('ui/handheld/android/ApplicationWindow');
-    } else {
-      Window = require('ui/handheld/ApplicationWindow');
-    }
-  }
-  new Window().open();
-})();
+var etSQL = Ti.UI.createLabel({
+	color: '#545861',
+  	font: { fontSize:12 },
+  	text: 'Arme su sentencia SQL',
+  	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+  	top: 30,
+  	width: Ti.UI.SIZE, height: '10%'
+});
+
+ventana1.add(etSQL);
+
+var btnConsultar = Titanium.UI.createButton({
+   title: 'Consultar',
+   top: 10,
+});
+
+ventana1.add(btnConsultar);
+
+var ventana2 = Ti.UI.createWindow({
+	title: 'ventana 2',
+	backgroundColor: '#F2F7FC',
+});
+
+var pestana2 = Ti.UI.createTab({
+	title: 'Resultado SQL',
+	window: ventana2,
+});
+
+miTabGroup.addTab(pestana1);
+miTabGroup.addTab(pestana2);
+
+miTabGroup.open();
+
+btnConsultar.addEventListener('click', function(e) {
+	var tabAdministrados = miTabGroup.getTabs();
+	Ti.API.info(miTabGroup.getActiveTab());
+	Ti.API.info(tabAdministrados[1]);
+	Ti.API.info(tabAdministrados[0]);
+	miTabGroup.setActiveTab(tabAdministrados[1]);
+});
